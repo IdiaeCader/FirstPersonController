325555f0e83c76bcaec12a8882caeabdb2fdb0e3
IdiaeCader <TZanderM@gmail.com>
2026-01-07 11:01:01 +0000

    Final Assets

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
@@ -42,7 +41,6 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Menu");
     }
     public void QuitGame()
     {
